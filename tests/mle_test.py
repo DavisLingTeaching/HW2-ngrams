8ea422e08dbf3eed3adc803f32f35600a750b141
forrestdavis <fdavis@colgate.edu>
2023-10-23 12:07:36 -0400

    Fixed issue with mle test

--- tests/mle_test.py
+++ tests/mle_test.py
@@ -38,12 +38,12 @@ def testMLE():
 
     data = "the kjdflkajs of the book's plot was weird and gothic"
 
-    goldNGrams = {'1gram': {'the': 2, '<unk>': 5, 'of': 1, 'book': 1, 'was': 1, 'and': 1, '</s>': 1}, '2gram': {'<s>': {'the': 1}, 'the': {'<unk>': 1, 'book': 1}, '<unk>': {'of': 1, '<unk>': 1, 'was': 1, 'and': 1, '</s>': 1}, 'of': {'the': 1}, 'book': {'<unk>': 1}, 'was': {'<unk>': 1}, 'and': {'<unk>': 1}}, '3gram': {'<s> <s>': {'the': 1}, '<s> the': {'<unk>': 1}, 'the <unk>': {'of': 1}, '<unk> of': {'the': 1}, 'of the': {'book': 1}, 'the book': {'<unk>': 1}, 'book <unk>': {'<unk>': 1}, '<unk> <unk>': {'was': 1}, '<unk> was': {'<unk>': 1}, 'was <unk>': {'and': 1}, '<unk> and': {'<unk>': 1}, 'and <unk>': {'</s>': 1}}}
+    goldNGrams_1 = {'1gram': {'the': 2, '<unk>': 5, 'of': 1, 'book': 1, 'was': 1, 'and': 1, '</s>': 1}, '2gram': {'<s>': {'the': 1}, 'the': {'<unk>': 1, 'book': 1}, '<unk>': {'of': 1, '<unk>': 1, 'was': 1, 'and': 1, '</s>': 1}, 'of': {'the': 1}, 'book': {'<unk>': 1}, 'was': {'<unk>': 1}, 'and': {'<unk>': 1}}, '3gram': {'<s> <s>': {'the': 1}, '<s> the': {'<unk>': 1}, 'the <unk>': {'of': 1}, '<unk> of': {'the': 1}, 'of the': {'book': 1}, 'the book': {'<unk>': 1}, 'book <unk>': {'<unk>': 1}, '<unk> <unk>': {'was': 1}, '<unk> was': {'<unk>': 1}, 'was <unk>': {'and': 1}, '<unk> and': {'<unk>': 1}, 'and <unk>': {'</s>': 1}}}
 
-    compareGrams(data, goldNGrams, model.mle(data, {}))
+    compareGrams(data, goldNGrams_1, model.mle(data, {}))
 
     data = "the cat sleeps. And the man snores!"
     goldNGrams = {'1gram': {'the': 4, '<unk>': 6, 'of': 1, 'book': 1, 'was': 1, 'and': 2, '</s>': 3, 'cat': 1, 'sleeps': 1, '.': 1, 'man': 1, '!': 1}, '2gram': {'<s>': {'the': 2, 'and': 1}, 'the': {'<unk>': 1, 'book': 1, 'cat': 1, 'man': 1}, '<unk>': {'of': 1, '<unk>': 1, 'was': 1, 'and': 1, '</s>': 1, '!': 1}, 'of': {'the': 1}, 'book': {'<unk>': 1}, 'was': {'<unk>': 1}, 'and': {'<unk>': 1, 'the': 1}, 'cat': {'sleeps': 1}, 'sleeps': {'.': 1}, '.': {'</s>': 1}, 'man': {'<unk>': 1}, '!': {'</s>': 1}}, '3gram': {'<s> <s>': {'the': 2, 'and': 1}, '<s> the': {'<unk>': 1, 'cat': 1}, 'the <unk>': {'of': 1}, '<unk> of': {'the': 1}, 'of the': {'book': 1}, 'the book': {'<unk>': 1}, 'book <unk>': {'<unk>': 1}, '<unk> <unk>': {'was': 1}, '<unk> was': {'<unk>': 1}, 'was <unk>': {'and': 1}, '<unk> and': {'<unk>': 1}, 'and <unk>': {'</s>': 1}, 'the cat': {'sleeps': 1}, 'cat sleeps': {'.': 1}, 'sleeps .': {'</s>': 1}, '<s> and': {'the': 1}, 'and the': {'man': 1}, 'the man': {'<unk>': 1}, 'man <unk>': {'!': 1}, '<unk> !': {'</s>': 1}}}
 
-    compareGrams(data, goldNGrams, model.mle(data, goldNGrams))
+    compareGrams(data, goldNGrams, model.mle(data, goldNGrams_1))
 
